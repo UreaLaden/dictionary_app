0483f43431d75ae98c92bfef7bb6de22d1877aa4
Leaundrae Mckinney <pytechdrae@gmail.com>
2023-01-24 00:47:16 -0500

    Small bug fix

--- ClientApp/src/components/Layout.css.js
+++ ClientApp/src/components/Layout.css.js
@@ -2,7 +2,7 @@ import {mergeStyleSets} from "@fluentui/react";
 
 export const styles = mergeStyleSets({
     container:{
-        margin:"1%",
+        margin:"auto",
         fontSize:"1em",
         height:"100%",
         width:"100%",
